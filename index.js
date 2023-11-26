1a078fe444a13e3fee8579ec3cdc3d8e2091480f
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-26 10:20:18 +0600

    project_initialized

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+// const nodemailer = require('nodemailer');
+const morgan = require("morgan");
+const port = process.env.PORT || 8000;
+
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wlf4d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    console.log("going on");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Pollinate Server..");
+});
+
+app.listen(port, () => {
+  console.log(`Pollinate Server is running on port ${port}`);
+});
