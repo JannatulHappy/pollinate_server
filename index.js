1d45c90f05a5b53fbc63ad842128b7e68ba35624
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-30 13:42:46 +0600

    up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
+const app = express();
+
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
